d0b56aab41591cb99a922bb605e3418edd4cda3f
Brian Wheatman <bwheatman@gmail.com>
2022-10-16 09:57:21 -0400

    adding newlines to the end of files

--- ParallelTools/parallel_scan.hpp
+++ ParallelTools/parallel_scan.hpp
@@ -8,7 +8,6 @@ namespace ParallelTools {
 template <typename Range, typename Value, typename Scan, typename Combine>
 Value parallel_scan(const Range &range, const Value &identity, const Scan &scan,
                     const Combine &combine, size_t min_serial_size) {
-                        //TODO
-
-                    }
+  // TODO
+}
 } // namespace ParallelTools
